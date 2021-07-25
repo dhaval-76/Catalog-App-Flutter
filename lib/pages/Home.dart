5dc87055058be906adb8dd14bab1998fab8d3587
Dhruvsolanki345 <dhruvsolanki753@gmail.com>
2021-07-25 13:32:27 +0530

    List View

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -1,8 +1,35 @@
 import "package:flutter/material.dart";
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:flutter_app1/models/catalog.dart';
 import 'package:flutter_app1/widgets/drawer.dart';
+import 'package:flutter_app1/widgets/item.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,10 +37,18 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome guys"),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: CatalogModel.items.isNotEmpty
+            ? ListView.builder(
+                itemCount: CatalogModel.items.length,
+                itemBuilder: (context, index) => ItemWidget(
+                  item: CatalogModel.items[index],
+                ),
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
