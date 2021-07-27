62931e621db896b1adedb397e4bc840e3df4f717
Dhruvsolanki345 <dhruvsolanki753@gmail.com>
2021-07-27 11:49:45 +0530

    Vx State Added updated

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -4,24 +4,23 @@ import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
   // Catalog field
-  late CatalogModel _catalog;
+  late CatalogModel catalog;
 
   // Collection of IDs - store Ids of each item
   final List<int> _itemIds = [];
 
-  CatalogModel get catalog => _catalog;
+  // CatalogModel get catalog => _catalog;
 
-  set catalog(CatalogModel newCatalog) {
-    _catalog = newCatalog;
-  }
+  // set catalog(CatalogModel newCatalog) {
+  //   _catalog = newCatalog;
+  // }
 
   // Get items in cart
-  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
+  List<Item> get items => _itemIds.map((id) => catalog.getById(id)).toList();
 
   // Get total price
   num get totalPrice =>
       items.fold(0, (total, current) => total + current.price);
-
 }
 
 class AddMutation extends VxMutation<MyStore> {
